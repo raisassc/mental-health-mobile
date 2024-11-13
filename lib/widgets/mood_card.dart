d1c0a08a5f10bde60e54eb47e3d32c15b3e78cd9
raisassc <sakila.raisa@gmail.com>
2024-11-13 14:49:09 +0700

    menyelesaikan tutor 8

--- lib/widgets/mood_card.dart
+++ lib/widgets/mood_card.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:mental_health_tracker/screens/moodentry_form.dart';
+import 'package:mental_health_tracker/screens/list_moodentry.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
+import 'package:mental_health_tracker/screens/login.dart';
 
 class ItemHomepage {
      final String name;
@@ -18,6 +22,7 @@ class ItemCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
     return Material(
       // Menentukan warna latar belakang dari tema aplikasi.
       color: Theme.of(context).colorScheme.secondary,
@@ -25,20 +30,61 @@ class ItemCard extends StatelessWidget {
       borderRadius: BorderRadius.circular(12),
       
       child: InkWell(
-        // Aksi ketika kartu ditekan.
-        onTap: () {
-          // Menampilkan pesan SnackBar saat kartu ditekan.
-          ScaffoldMessenger.of(context)
-            ..hideCurrentSnackBar()
-            ..showSnackBar(
-              SnackBar(content: Text("Kamu telah menekan tombol ${item.name}!"))
-            );
+// Area responsif terhadap sentuhan
+      onTap: () async {
+        // Memunculkan SnackBar ketika diklik
+        ScaffoldMessenger.of(context)
+          ..hideCurrentSnackBar()
+          ..showSnackBar(SnackBar(
+              content: Text("Kamu telah menekan tombol ${item.name}!")));
+
+        // Navigate ke route yang sesuai (tergantung jenis tombol)
         if (item.name == "Tambah Mood") {
-        Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => MoodEntryFormPage(),
-                    ));}},
+          // TODO: Gunakan Navigator.push untuk melakukan navigasi ke MaterialPageRoute yang mencakup TrackerFormPage.
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => MoodEntryFormPage(),
+              ));
+        }
+
+          else if (item.name == "Lihat Mood") {
+            Navigator.push(context,
+                MaterialPageRoute(
+                    builder: (context) => const MoodEntryPage()
+                ),
+            );
+        }
+
+
+        // statement if sebelumnya
+        // tambahkan else if baru seperti di bawah ini
+        else if (item.name == "Logout") {
+            final response = await request.logout(
+                // TODO: Ganti URL dan jangan lupa tambahkan trailing slash (/) di akhir URL!
+                "http://127.0.0.1:8000/auth/logout/");
+            String message = response["message"];
+            if (context.mounted) {
+                if (response['status']) {
+                    String uname = response["username"];
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: Text("$message Sampai jumpa, $uname."),
+                    ));
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => const LoginPage()),
+                    );
+                } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                            content: Text(message),
+                        ),
+                    );
+                }
+            }
+        }
+      },
+
         // Container untuk menyimpan Icon dan Text
         child: Container(
           padding: const EdgeInsets.all(8),
